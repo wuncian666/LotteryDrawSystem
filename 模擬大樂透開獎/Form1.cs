60b22d8e77cf1ef1db242e4e2c9426232b5a12b4
wuncian <wuncian666@gmail.com>
2024-06-22 21:37:20 +0800

    加入專案檔案。

---
+++ 模擬大樂透開獎/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 模擬大樂透開獎
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i <49; i++)
+            {
+                Button button = new Button();
+                button.Text = (i+1).ToString();
+                button.Width = 50;
+                button.Height = 50;
+                button.Click += Number_Click;
+                flowLayoutPanel1.Controls.Add(button);
+            }
+        }
+
+        int limit = 6;
+        List<Button> buttonList = new List<Button>();
+        private void Number_Click(object sender, EventArgs e)
+        {
+            if (buttonList.Count < limit)
+            {
+                Button button = (sender) as Button;
+                buttonList.Add(button);
+
+                PrintNum();
+
+                button.Enabled = false;
+            } 
+            else
+            {
+                MessageBox.Show("超過六個數字", "B", MessageBoxButtons.OKCancel);
+            }
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button lastButton = buttonList.LastOrDefault();
+
+            if (lastButton != null)
+            {
+                lastButton.Enabled = true;
+                buttonList.Remove(lastButton);
+
+                PrintNum();
+            }
+        }
+
+        private void PrintNum()
+        {
+            textBox1.Clear();
+
+            foreach (Button button in buttonList)
+            {
+                textBox1.Text += button.Text + " ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<String> buttonNameList = buttonList.Select(x => x.Text).ToList();
+            String result = LotteryDraw.GetLotteryResult(buttonNameList);
+            MessageBox.Show(result, "B", MessageBoxButtons.OKCancel);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<String> nums = LotteryDraw.RandomNumList();
+
+            String numsStr = "";
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (i < (nums.Count - 1))
+                {
+                    numsStr += nums[i] + ", ";
+                } else
+                {
+                    numsStr += nums[i];
+                }
+                    
+            }
+
+            textBox2.Text = numsStr;
+        }
+    }
+}
---
+++ 模擬大樂透開獎/LotteryDraw.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 模擬大樂透開獎
+{
+    internal class LotteryDraw
+    {
+        public static List<String> RandomNumList()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+
+            HashSet<String> set = new HashSet<String>();
+            while (set.Count < 6)
+            {
+                set.Add(random.Next(1, 50).ToString());
+            }
+            return set.ToList();
+        }
+
+        public static String GetLotteryResult(List<String> buttonNameList)
+        {
+            int num = 0;
+
+            List<String> randomNum = RandomNumList();
+            Console.WriteLine("Random" + randomNum.Count);
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(randomNum[i]);
+
+                if (randomNum.Contains(buttonNameList[i]))
+                {
+                    num++;
+                }
+            }
+
+            String specialNum = buttonNameList.Where(x => x.Contains(randomNum[5])).Select(x => x).FirstOrDefault();
+
+            return specialNum;
+
+            //MessageBox.Show(GetRewardResult(num, specialNum), "B", MessageBoxButtons.OKCancel);
+        }
+
+        private String GetRewardResult(int num, String specialNum)
+        {
+            Console.WriteLine($"特別號:{specialNum}");
+            switch (num)
+            {
+                case 2:
+                    return (specialNum != null) ? "七獎" : "未中獎";
+                case 3:
+                    return (specialNum != null) ? "普獎" : "六獎";
+                case 4:
+                    return (specialNum != null) ? "四獎" : "五獎";
+                case 5:
+                    return (specialNum != null) ? "二獎" : "三獎";
+                case 6:
+                    return "頭獎";
+                default:
+                    return "未中獎";
+            }
+        }
+    }
+}
